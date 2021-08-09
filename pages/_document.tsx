c59c4d2c8d123a8a93dc995c2129392476774e2a
natandias <natandias@pm.me>
2021-08-09 00:48:27 -0300

    Fix: correct errors catched on build

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -34,10 +34,10 @@ export default class MyDocument extends Document {
   }
   render() {
     return (
-      <Html>
+      <Html lang="pt-br">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
             rel="stylesheet"
